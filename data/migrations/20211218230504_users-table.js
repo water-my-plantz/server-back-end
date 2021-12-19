6be1ea690dfb0c3c397dbca3bda68d1e6811aed9
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2021-12-19 15:26:06 -0800

    more db

--- data/migrations/20211218230504_users-table.js
+++ data/migrations/20211218230504_users-table.js
@@ -1,27 +1,72 @@
 
 // If you add a table you have to npm run down, then npm run up
 
+// `user` can sign-up / create an account by providing a unique `username`, a valid mobile `phoneNumber` and a `password`. 
+
+// Production never has migrate down, only up.
 
 exports.up = async function (knex) {
     await knex.schema.createTable('users', table => {
-        table.increments('user_id') // auto-incrementing id; primary key     
+        table.increments() // auto-incrementing id; primary key     
         table.text('username', 128).unique().notNullable()
-        table.text('password').unique().notNullable()
-        table.text('phoneNumber').unique().notNullable()
-        table.text('species')
-        table.text('nickname')
-        table.text('h2oFrequency')
+        table.text('password').notNullable()
+        table.text('phoneNumber')
     })
+        .createTable('plants', table => {
+            table.text('phoneNumber')
+            table.text('species')
+            table.text('nickname')
+            table.text('h2oFrequency')
+        })
 };
 
 exports.down = async function (knex) {
-    await knex.schema.dropTableIfExists('users');
+    await knex.schema.dropTableIfExists('users')
+    await knex.schema.dropTableIfExists('plants')
+
 };
 
 // npm run up creates the db
 
-// `nickname`: String
-// `species` : String
-// `h2oFrequency`:          Type determined by implementation  ????????
-// `phoneNumber`
-// `password`.
+// FROM LAST BUILD:
+
+// exports.up = function (knex) {
+//     return knex.schema
+//         .createTable('users', (tbl) => {
+//             tbl.increments()
+//             tbl.string('username').unique().notNullable()
+//             tbl.string('password').notNullable()
+//             tbl.string('phone_number')
+//         })
+//         .createTable('plants', (tbl) => {
+//             tbl.increments()
+//             tbl.string('nickname')
+//             tbl.string('species')
+//             tbl.string('h2o_frequency')
+//             tbl.string('image')
+//         })
+//         .createTable('users_plants', (tbl) => {
+//             tbl.increments()
+//             tbl.integer('user_id')
+//                 .unsigned()
+//                 .notNullable()
+//                 .references('id')
+//                 .inTable('users')
+//                 .onDelete('CASCADE')
+//                 .onUpdate('CASCADE')
+//             tbl.integer('plant_id')
+//                 .unsigned()
+//                 .notNullable()
+//                 .references('id')
+//                 .inTable('plants')
+//                 .onDelete('CASCADE')
+//                 .onUpdate('CASCADE')
+//         })
+// }
+
+// exports.down = function (knex) {
+//     return knex.schema
+//         .dropTableIfExists('users_plants')
+//         .dropTableIfExists('plants')
+//         .dropTableIfExists('users')
+// }
