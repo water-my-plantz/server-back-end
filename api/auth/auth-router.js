15d4a0c9dd1742f8b170ec1e1d2dfcfb15b6ef1b
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2021-12-22 19:39:14 -0800

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const Users = require('./auth-model')
 
-// const { checkValidRegister } = require('../middleware/restricted');
+const { checkValidRegister } = require('../middleware/restricted');
 
 // const bcrypt = require('bcryptjs');                     // This goes wherever we use bcrypt
 // const jwt = require('jsonwebtoken');                    // installed this library // used in tokenbuilder
@@ -18,6 +18,24 @@ router.get('/', (req, res) => {
             res.status(500).json({ message: 'Failed to get users', error })
         })
 });
+
+
+router.post('/register', checkValidRegister, async (req, res) => {
+    const { username, password } = req.body;       // Take whatever the user types
+    const hash = bcrypt.hashSync(password, 8);     // Hashes the user's password
+    const user = { username, password: hash }      // Create a user object with the username and hashed password
+
+    try {
+        const createdUser = await Users.create(user) // create = Knex* db('users').insert(user)
+        console.log('createdUser', createdUser)
+        res.status(201).json(createdUser)            // json = createdUser
+    } catch (err) {
+        res.status(500).json({ message: 'Error registering user', err });
+    }
+})
+
+
+
 
 
 module.exports = router;
