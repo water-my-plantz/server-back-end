6c0392e38c8d2daa3435eb89511b3a0f2316a0de
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2021-12-27 14:59:51 -0800

    token working

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const Users = require('./auth-model')
+
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../../secrets'); // Some have BCRYPT_ROUNDS, not sure why
+
+
 
 const { checkValidRegister } = require('../middleware/restricted');
 
@@ -48,20 +53,17 @@ router.post('/register', checkValidRegister, async (req, res) => {
 
 
 
-
-
 router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    console.log('username', username) // Aurelius
-
+    const { username, password } = req.body;            // Aurelius 1234
 
     try {
-        const user = await Users.findBy({ username })   // console.log(user) = { id: 3, username: 'Cato', password: '1234' }
-
-        console.log('user', user) // object of id, username, password.
+        const user = await Users.findBy({ username })   // { id: 3, username: 'Aurelius', password: '1234' }
 
         if (user && bcrypt.compareSync(password, user.password)) {
-            res.status(200).json({ message: `Welcome ${user.username}`, user })
+
+            const token = generateToken(user);          // eyJhbGciOiJIUzI1NiIsInR5cCI6...
+            res.status(200).json({ message: `Welcome back ${user.username}!`, token })
+
         } else {
             res.status(401).json({ message: 'Invalid Credentials' })
         }
@@ -70,6 +72,22 @@ router.post('/login', async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 })
+
+function generateToken(user) {
+    const payload = {
+        sub: user.id,               // subject, normally the user id
+        username: user.username,    // Custom property
+        role: user.role,            // ????????????????????????
+    }
+    const options = {
+        expiresIn: '12d',
+    }
+    return jwt.sign(payload, JWT_SECRET, options); // JWT_SECRET is an import
+}
+
+
+
+
 
 
 
