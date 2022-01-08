75439dedb050c8f2710559715245f93aaeb194c0
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2022-01-08 14:56:25 -0800

    wip

--- data/migrations/20211218230504_users-table.js
+++ data/migrations/20211218230504_users-table.js
@@ -15,7 +15,7 @@ exports.up = async function (knex) {
     })
         .createTable('plants', table => {
             table.increments()
-            table.integer('plant_id').notNullable()
+            table.text('image')
             table.text('species')
             table.text('nickname')
             table.text('water_frequency')
